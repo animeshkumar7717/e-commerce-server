ece8adc567a1aa51f93d31f310934d3c6b5b8eb8
Animesh Kumar <animesh.kumar7717@gmail.com>
2024-05-07 16:13:13 +0530

    feat/backend-server: created all servers of backend!

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const authRouters = require('./routes/auth.route')
+const userRouters = require('./routes/user.route')
+const productRouters = require('./routes/product.route')
+const adminProductRouters = require('./routes/adminProduct.route')
+const cartRouters = require('./routes/cart.route')
+const cartItemRouters = require('./routes/cartItem.route')
+const orderRouters = require('./routes/order.routes')
+const adminOrderRouters = require('./routes/adminOrder.route')
+const reviewRouters = require('./routes/review.route')
+const ratingRouters = require('./routes/rating.route')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/auth', authRouters);
+app.use('/api/users', userRouters); // working 
+app.use('/api/product', productRouters);
+app.use('/api/admin/product', adminProductRouters);
+app.use('/api/cart', cartRouters);
+app.use('/api/cart_items', cartItemRouters);
+app.use('/api/orders', orderRouters);
+app.use('/api/admin/orders', adminOrderRouters);
+app.use('/api/reviews', reviewRouters);
+app.use('/api/ratings', ratingRouters);
+
+module.exports=app;
---
+++ src/models/order.model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const { Schema } = require('mongoose');
+
+const orderSchema = new Schema({
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:'users'
+    },
+    orderItems:[
+        {
+            type:mongoose.Types.ObjectId,
+            ref:'orderItems'
+        }
+    ],
+    orderDate:{
+        type: Date,
+        require: true
+    },
+    deliveryDate:{
+        type: Date
+    },
+    shippingAddress:{
+        type:mongoose.Types.ObjectId,
+        ref:'addresses'
+    },
+    paymentDetails:{
+        paymentMethod:{
+            type:String
+        },
+        transactionId:{
+            type:String
+        },
+        paymentId:{
+            type:String
+        },
+        paymentStatus:{
+            type:String,
+            default:'PENDING'
+        }
+    },
+    discount:{
+        type:Number,
+        required:true
+    },
+    orderStatus:{
+        type:String,
+        required:true,
+        default:'PENDING'
+    },
+    totalItem:{
+        type:Number,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default: Date.now()
+    }
+})
+
+const Order = mongoose.model('orders', orderSchema);
+
+module.exports = Order;
